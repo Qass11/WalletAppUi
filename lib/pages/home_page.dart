bb7ca266a9351f13089182e14706cf626c3c98fb
Qassim Kurdi <327392@student.mboutrecht.nl>
2022-08-19 13:39:13 +0200

    List/

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
+import 'package:walletappui/util/my_button.dart';
 import 'package:walletappui/util/my_card.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +13,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //Pagecontroller
+
+  final _controller = PageController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -61,34 +66,129 @@ class _HomePageState extends State<HomePage> {
               height: 200,
               child: PageView(
                 scrollDirection: Axis.horizontal,
+                controller: _controller,
                 children: [
                   MyCard(
                     balamce: 5250.25,
                     cardNumber: 12345678,
                     expiryMonth: 10,
                     expiryYear: 24,
-                    color: Colors.deepPurple[400],
+                    color: Colors.deepPurple[300],
                   ),
                   MyCard(
                     balamce: 3320.10,
                     cardNumber: 12345678,
                     expiryMonth: 11,
                     expiryYear: 23,
-                    color: Colors.blue[400],
+                    color: Colors.blue[300],
                   ),
                   MyCard(
                     balamce: 530.00,
                     cardNumber: 12345678,
                     expiryMonth: 12,
                     expiryYear: 22,
-                    color: Colors.green[400],
+                    color: Colors.green[300],
                   ),
                 ],
               ),
             ),
+
+            SizedBox(height: 25),
+
+            SmoothPageIndicator(
+              controller: _controller,
+              count: 3,
+              effect: ExpandingDotsEffect(
+                activeDotColor: Colors.grey.shade800,
+              ),
+            ),
+
+            SizedBox(height: 25),
+
             // 3 buttons -> send + pay + bill
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 25.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  // Send Button
+                  MyButton(
+                    iconImagePath: 'lib/icons/send-money.png',
+                    buttonText: 'Send',
+                  ),
+
+                  // Pay Button
+                  MyButton(
+                    iconImagePath: 'lib/icons/credit-card.png',
+                    buttonText: 'Pay',
+                  ),
+
+                  // Bills Button
+
+                  MyButton(
+                    iconImagePath: 'lib/icons/bill.png',
+                    buttonText: 'Bills',
+                  ),
+                ],
+              ),
+            ),
 
             // Column -> states + transactions
+            Padding(
+              padding: const EdgeInsets.all(25.0),
+              child: Column(
+                children: [
+                  // Statistics
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(
+                        children: [
+                          // Icon
+                          Container(
+                            height: 80,
+                            padding: EdgeInsets.all(12),
+                            decoration: BoxDecoration(
+                              color: Colors.grey[100],
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                            child: Image.asset('lib/icons/statistics.png'),
+                          ),
+                          SizedBox(
+                            width: 20,
+                          ),
+
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                'Statistics',
+                                style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 20,
+                                ),
+                              ),
+                              SizedBox(
+                                height: 8,
+                              ),
+                              Text(
+                                'Payments and Icon',
+                                style: TextStyle(
+                                  fontSize: 16,
+                                  color: Colors.grey[600],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      Icon(Icons.arrow_forward_ios),
+                    ],
+                  ),
+                  // Transaction
+                ],
+              ),
+            ),
           ],
         ),
       ),
